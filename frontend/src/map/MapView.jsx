739f352cbfaaad782a131e7c67965bfe53acc9b4
Nurasyl <bnur03@icloud.com>
2026-09-23 09:33:52 +0500

    white mode

--- frontend/src/map/MapView.jsx
+++ frontend/src/map/MapView.jsx
@@ -181,7 +181,7 @@ export default function MapView({ analysis, setPolygon, mapRef }) {
         style={{ height: "100%", width: "100%", background: "#060810" }}
         zoomControl={false}
       >
-        {/* Рабочая рабочая тёмная подложка от CARTO Dark Matter без API ключей */}
+
         <TileLayer
           url="https://basemaps.cartocdn.com/rastertiles/dark_matter_gl_style/{z}/{x}/{y}.png?key=cb1_3twd_1_507927c60b9eda9645dcdb95"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
